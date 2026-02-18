678477f13a421e4af7dd0e162b5606206d319ed5
Villy Siu <villysiu@gmail.com>
2026-02-18 13:37:37 -0800

    preload data into context

---
+++ src/components/MenuContext.jsx
@@ -0,0 +1,67 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+const MenuContext = createContext();
+
+export const MenuProvider = ({ children }) => {
+    const backendApi = 'http://localhost:3000'
+    const [menuitems, setMenuitems] = useState([])
+    const [milks, setMilks] = useState([])
+    const [sizes, setSizes] = useState([])
+    const [temperatures, setTemperatures] = useState([])
+    const [sugars, setSugars] = useState([])
+    const [loading, setLoading] = useState(true);
+
+    const fetchHelper = async (apiUrl) => {
+        const response = await fetch(apiUrl)
+
+        if(!response.ok){
+            const errorText = await response.text();
+            throw new Error(`Error ${response.status}: ${errorText}`)
+        }
+        return response.json()
+    }
+
+    useEffect(() => {
+        const fetchMenuitemData = async () => {
+        try {
+            const [menuitemData, milkData, sizeData, temperatureData, sugarData] = await Promise.all([
+                fetchHelper(`${backendApi}/api/v1/menuitems`),
+                fetchHelper(`${backendApi}/api/v1/milks`),
+                fetchHelper(`${backendApi}/api/v1/sizes`),
+                fetchHelper(`${backendApi}/api/v1/temperatures`),
+                fetchHelper(`${backendApi}/api/v1/sugars`),
+            ]);
+
+            setMenuitems(menuitemData.menuitems)
+            setMilks(milkData.milks);
+            setSizes(sizeData.sizes)
+            setTemperatures(temperatureData);
+            setSugars(sugarData)
+
+        } catch (error) {
+            console.error("Error fetching menu data:", error);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        fetchMenuitemData();
+    }, []);
+
+    return (
+        <MenuContext.Provider
+        value={{
+            menuitems,
+            milks,
+            sizes,
+            temperatures,
+            sugars,
+            loading,
+        }}
+        >
+        {children}
+        </MenuContext.Provider>
+    );
+    };
+
+    export const useMenu = () => useContext(MenuContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { MenuProvider } from './components/MenuContext'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <MenuProvider>
+      <App />
+    </MenuProvider>
+  </BrowserRouter>
 )
