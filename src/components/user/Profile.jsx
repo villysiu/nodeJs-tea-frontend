c85120ba09b46d598ba63fc6bbf29aff4ce19a99
Villy Siu <villysiu@gmail.com>
2026-02-22 14:24:54 -0800

    account

--- src/components/user/Profile.jsx
+++ src/components/user/Profile.jsx
@@ -1,6 +1,18 @@
+import { useAuth } from '../context/AuthContext'
+import { Container, Row, Col } from 'react-bootstrap'
 const Profile = () => {
+    const {user} = useAuth();
+    console.log(user)
     return ( 
-        <> user secret  info</>
+        <Container className="d-flex justify-content-center">
+            <Row>
+                <Col xs={12}><h4>Account </h4></Col>
+      
+            <Col xs={3} className='text-start'>name:</Col>
+            <Col xs={3}className='text-start'>{user.name}</Col>
+            <Col></Col>
+            </Row>
+        </Container>
     )
 }
 export default Profile
