38bc4355b4c3468e1566dc6d7d3854b3250dc478
Villy Siu <villysiu@gmail.com>
2026-02-22 13:50:05 -0800

    converted login and sipup to modal

--- src/components/routes/ProtectedRoute.jsx
+++ src/components/routes/ProtectedRoute.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Navigate, useLocation } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import { useAlert } from '../context/AlertContext'
 
 const ProtectedRoute = ({children}) => {
-    const { user } = useAuth();
+    const { user, loading, setShow } = useAuth();
+    const { showAlert } = useAlert();
     const location = useLocation();
 
-    if(!user)
-        return <Navigate to='../login' state={{ from: location }} replace />;
+    useEffect(() => {
+        if(loading)
+            return;
+        if (!user) {
+            setShow('login')
+            showAlert("Authentication required", "warning");
+        }
+    }, [user, showAlert, loading]); 
+
+    if(!user) return;
 
     return children
 }
